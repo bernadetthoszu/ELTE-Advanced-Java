6fe3300211b9d7cfe8fb329a6ed8a01d2d94e07b
913-Hoszu-Bernadett <91715605+bernadetthoszu@users.noreply.github.com>
2023-03-23 23:18:34 +0100

    Weekday customized 1

--- Javaverse/src/time/Weekday.java
+++ Javaverse/src/time/Weekday.java
@@ -1,16 +1,51 @@
 package time;
 
+import java.util.Arrays;
+
 public enum Weekday {
-	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+	MONDAY(new String[]{"Hetfo", "Lunes"}), TUESDAY("Kedd", "Martes"), WEDNESDAY("Szerda", "Miercoles"), THURSDAY("Csutortok", "Jueves"), FRIDAY("Pentek", "Viernes"), SATURDAY, SUNDAY;
+	
+	private String[] langs = {"hu", "es"};
+	
+	private String[] names;
+	//private String lang1Name;
+	//private String lang2Name;
+	
+	private Weekday() {
+		//this.lang1Name = "nap";
+		//this.lang2Name = "dia";
+	}
+	
+	//private Weekday(String[] names) {
+	// varargs
+	private Weekday(String... names) {
+		//this.lang1Name = lang1Name; 
+		//this.lang2Name = lang2Name;
+		this.names = names;
+	}
 	
 	public Weekday getNextDay(){
 		return getDaysAfter(1);
 	}
 	
+//	public String getLang1Name() {
+//		return lang1Name;
+//	}
+//
+//
+//	public String getLang2Name() {
+//		return lang2Name;
+//	}
+
 	public Weekday getDaysAfter(int dayCount){
 		//int weekdayCount = 7;
 		int weekdayCount = Weekday.values().length;
 		int nextDayIdx = (this.ordinal() + dayCount) % weekdayCount;  //magical constant
 		return Weekday.values()[nextDayIdx];
+	}
+	
+	public String getName(String lang) {
+		int langIdx = Arrays.binarySearch(langs, lang);
+		return names[langIdx];
 	}
 }
--- Javaverse/src/time/WeekdayMain.java
+++ Javaverse/src/time/WeekdayMain.java
@@ -4,5 +4,11 @@ public class WeekdayMain {
 	public static void main(String[] args) {
 		System.out.println(Weekday.TUESDAY.getNextDay());
 		System.out.println(Weekday.TUESDAY.getNextDay().getNextDay());
+		
+//		System.out.println(Weekday.TUESDAY.getLang1Name());
+//		System.out.println(Weekday.TUESDAY.getLang2Name());
+		
+		System.out.println(Weekday.TUESDAY.getName("hu"));
+		System.out.println(Weekday.TUESDAY.getName("es"));
 	}
 }
